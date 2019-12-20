934181ae01ffd812db8b69386776d6c0e388c64d
Chris Cruz <christopherc1331@gmail.com>
2019-12-19 20:53:41 -0800

    Edited migration file. Committing before migrate:latest

--- data/migrations/20191218215437_data.js
+++ data/migrations/20191218215437_data.js
@@ -1,11 +1,63 @@
 exports.up = function(knex) {
-	return knex.schema.createTable('accounts', (tbl) => {
-		tbl.increments();
-		tbl.string('username').unique().notNullable();
-		tbl.string('password').notNullable();
-	});
+  return knex.schema
+    .createTable("accounts", tbl => {
+      tbl.increments();
+      tbl
+        .string("username")
+        .unique()
+        .notNullable();
+      tbl.string("password").notNullable();
+    })
+
+    .createTable("logs", tbl => {
+      tbl.increments();
+      tbl.timestamp("created_at").defaultTo(knex.fn.now());
+      tbl
+        .float("time_spent")
+        .unsigned()
+        .notNullable();
+      tbl.string("location").notNullable();
+      tbl
+        .integer("bait_id")
+        .unsigned()
+        .notNullable()
+        .references("bait.id");
+      tbl
+        .integer("accounts_id")
+        .unsigned()
+        .notNullable()
+        .references("accounts.id");
+    })
+
+    .createTable("bait", tbl => {
+      tbl.increments();
+      tbl
+        .string("bait_name")
+        .unique()
+        .notNullable();
+    })
+
+    .createTable("logs_bait", tbl => {
+      tbl
+        .integer("logs_id")
+        .unsigned()
+        .notNullable()
+        .references("logs.id");
+
+      tbl
+        .integer("bait_id")
+        .unsigned()
+        .notNullable()
+        .references("bait.id");
+
+      tbl.primary(["logs_id", "bait_id"]);
+    });
 };
 
 exports.down = function(knex) {
-	return knex.schema.dropTableIfExists('accounts');
+  return knex.schema
+    .dropTableIfExists("logs_bait")
+    .dropTableIfExists("bait")
+    .dropTableIfExists("logs")
+    .dropTableIfExists("accounts");
 };
