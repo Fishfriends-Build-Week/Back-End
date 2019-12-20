658bac25ef6d79db77fd4ed1069acb33cbcdf7f7
Chris Cruz <christopherc1331@gmail.com>
2019-12-19 21:04:21 -0800

    Finished TripLogsModel.js file

--- TripLogs/TripLogsModel.js
+++ TripLogs/TripLogsModel.js
@@ -1,2 +1,25 @@
-const express = require("expres");
-const 
+const db = require("../data/dbConfig");
+
+module.exports = {
+  add,
+  findBy,
+  findById
+};
+
+async function add(log) {
+  const [id] = await db("logs").insert(log, "id");
+
+  return db("accounts")
+    .where({ id })
+    .first();
+}
+
+function findBy(logName) {
+  return db("logs").where(logName);
+}
+
+function findById(id) {
+  return db("logs")
+    .where({ id })
+    .first();
+}
