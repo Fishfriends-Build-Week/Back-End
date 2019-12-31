86b1afaf1c3e3e9291e4e8e30d8265cc003471d9
Chris Cruz <christopherc1331@gmail.com>
2019-12-30 20:46:41 -0800

    Added req to accounts route

--- Users/UsersRoutes.js
+++ Users/UsersRoutes.js
@@ -7,7 +7,7 @@ const db = require("./UsersModel");
 
 const router = express.Router();
 
-router.get("/", res => {
+router.get("/", (req, res) => {
   db.find()
     .then(users => {
       res.status(200).json({ success: true, users });
@@ -33,9 +33,11 @@ router.post("/register", (req, res) => {
 
 router.post("/login", (req, res) => {
   let { username, password } = req.body;
+  console.log("user from login route", req.body);
   db.findBy({ username })
     .first()
     .then(login => {
+      console.log("login returned from db findBy", login);
       if (login && bcrypt.compareSync(password, login.password)) {
         const token = Token(login);
 
--- Users/authentication/authMiddleWare.js
+++ Users/authentication/authMiddleWare.js
@@ -1,21 +1,21 @@
-const jwt = require('jsonwebtoken');
-const secret = require('./tokenSecret');
+const jwt = require("jsonwebtoken");
+const secret = require("./tokenSecret");
 
 module.exports = (req, res, next) => {
-    const token = req.headers.authorization;
-
-    if (token) {
-        jwt.verify(token, secret.secret, (err, decodedToken) => {
-            if (err) {
-                res.status(401).json({ message: 'login'});
-            } else {
-                req.user = {
-                    username: decodedToken.username
-                };
-                next()
-            }
-        });
-    } else {
-        res.status(401).json({message: "no token"})
-    }
-}
+  const token = req.headers.authorization;
+  console.log("token", token);
+  if (token) {
+    jwt.verify(token, secret.secret, (err, decodedToken) => {
+      if (err) {
+        res.status(401).json({ message: "login" });
+      } else {
+        req.user = {
+          username: decodedToken.username
+        };
+        next();
+      }
+    });
+  } else {
+    res.status(401).json({ message: "no token" });
+  }
+};
