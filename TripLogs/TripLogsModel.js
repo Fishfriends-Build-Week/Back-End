cbc8deb7f090f11c2734ef22585c518b90468838
Chris Cruz <christopherc1331@gmail.com>
2020-01-06 21:53:40 -0800

    Changes to location and logs routes

--- TripLogs/TripLogsModel.js
+++ TripLogs/TripLogsModel.js
@@ -16,14 +16,25 @@ async function add(log) {
 }
 
 function find() {
-  return db("logs_bait as lb")
-    .join("logs as l, l.log_id, lb.log_id")
-    .join("bait as b, b.bait_id, lb.bait_id")
-    .orderBy("l.log_id, b.bait_id");
+  //   SELECT *
+  // FROM logs
+  // JOIN logs_bait ON logs_bait.log_id = logs.log_id
+  // JOIN bait ON logs_bait.bait_id = bait.bait_id
+  // ORDER BY logs.log_id
+
+  return db("logs as l")
+    .join("logs_bait as lb", "lb.log_id", "l.log_id")
+    .join("bait as b", "lb.bait_id", "b.bait_id")
+    .orderBy("l.log_id");
+
+  // return db("logs_bait as lb")
+  //   .join("logs as l, l.log_id, lb.log_id")
+  //   .join("bait as b, b.bait_id, lb.bait_id")
+  //   .orderBy("l.log_id, b.bait_id");
 }
 
 function findByLocation(location) {
-  return db("logs").where(location);
+  return db("logs").where("location_name", location);
 }
 
 function findById(log_id) {
--- Users/authentication/authMiddleWare.js
+++ Users/authentication/authMiddleWare.js
@@ -3,7 +3,6 @@ const secret = require("./tokenSecret");
 
 module.exports = (req, res, next) => {
   const token = req.headers.authorization;
-  console.log("token", token);
   if (token) {
     jwt.verify(token, secret.secret, (err, decodedToken) => {
       if (err) {
