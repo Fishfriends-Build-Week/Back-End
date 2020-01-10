920e742dca4257b012d3ff4653029dbdc9c564c7
Chris Cruz <christopherc1331@gmail.com>
2020-01-09 19:04:09 -0800

    Updated migrations file with fish table

--- data/migrations/20191218215437_data.js
+++ data/migrations/20191218215437_data.js
@@ -25,6 +25,15 @@ exports.up = function(knex) {
         .unique()
         .notNullable();
     })
+
+    .createTable("fish", tbl => {
+      tbl.increments("fish_id");
+      tbl
+        .string("fish_name")
+        .unique()
+        .notNullable();
+    })
+
     .createTable("logs", tbl => {
       tbl.increments("log_id");
       tbl.timestamp("created_at").defaultTo(knex.fn.now());
@@ -64,14 +73,37 @@ exports.up = function(knex) {
         .onDelete("CASCADE")
         .onUpdate("CASCADE");
 
-      tbl.primary(["log_id", "bait_id"]);
+      tbl
+        .primary(["log_id", "bait_id"])
+
+        .createTable("logs_fish", tbl => {
+          tbl
+            .integer("log_id")
+            .unsigned()
+            .notNullable()
+            .references("logs.log_id")
+            .onDelete("CASCADE")
+            .onUpdate("CASCADE");
+
+          tbl
+            .integer("fish_id")
+            .unsigned()
+            .notNullable()
+            .references("fish.fish_id")
+            .onDelete("CASCADE")
+            .onUpdate("CASCADE");
+
+          tbl.primary(["log_id", "fish_id"]);
+        });
     });
 };
 
 exports.down = function(knex) {
   return knex.schema
+    .dropTableIfExists("logs_fish")
     .dropTableIfExists("logs_bait")
     .dropTableIfExists("logs")
+    .dropTableIfExists("fish")
     .dropTableIfExists("bait")
     .dropTableIfExists("locations")
     .dropTableIfExists("accounts");
